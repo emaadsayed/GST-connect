0156d2babda147c89f3648dd0f752e11fb2afb97
Sahil Raut <sahilraut0801@gmail.com>
2021-01-24 13:19:59 +0530

    UI

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,27 @@
-module.exports.index = async (req, res) => {
-    res.send('respond with a resource');
+module.exports.connect = async (req, res) => {
+    res.render('connect');
+}
+
+module.exports.projects = async (req, res) => {
+    res.render('projects');
+}
+
+module.exports.myprojects = async (req, res) => {
+    res.render('myprojects');
+}
+
+module.exports.createproject = async (req, res) => {
+    res.render('createproject');
+}
+
+module.exports.books = async (req, res) => {
+    res.render('books');
+}
+
+module.exports.mybooks = async (req, res) => {
+    res.render('mybooks');
+}
+
+module.exports.createbook = async (req, res) => {
+    res.render('createbook');
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,14 @@ var router = express.Router();
 const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', userController.index);
+router.get('/connect', userController.connect);
+
+router.get('/projects', userController.projects);
+router.get('/myprojects', userController.myprojects);
+router.get('/createproject', userController.createproject);
+
+router.get('/books', userController.books);
+router.get('/mybooks', userController.mybooks);
+router.get('/createbook', userController.createbook);
 
 module.exports = router;
